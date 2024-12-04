cc6c7ca3b228f5c46589ef4e787cd1d9236b7e71
DollyCytroz <kauemcarreiro11@gmail.com>
2024-12-03 21:01:33 -0300

    Site com o banco

--- rap_news (1).sql
+++ rap_news (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30/11/2024 às 20:14
+-- Tempo de geração: 04/12/2024 às 00:59
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
@@ -33,8 +33,17 @@ CREATE TABLE `noticias` (
   `autor` varchar(100) NOT NULL,
   `texto` text NOT NULL,
   `imagem` varchar(255) DEFAULT NULL,
-  `status` enum('pendente','aprovada','negada') DEFAULT 'pendente'
+  `status` enum('pendente','aprovada','negada') DEFAULT 'pendente',
+  `aprovado` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `noticias`
+--
+
+INSERT INTO `noticias` (`id`, `titulo`, `autor`, `texto`, `imagem`, `status`, `aprovado`) VALUES
+(2, 'Drake x Kendrick', 'Junior', 'O negão ficou puto com o canadense, o negao fez diss e o canadense tbm, canadense levou um pau', '674cbd645144f.webp', 'aprovada', 1),
+(9, 'Uma brisa mto loca', 'Ryan', 'Mto loucura meo', '98e7d11f7bf0bb5f2185be4cfa01b99c.jpg', 'pendente', 1);
 
 -- --------------------------------------------------------
 
@@ -45,7 +54,7 @@ CREATE TABLE `noticias` (
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
   `tipo` enum('admin','escritor') NOT NULL,
   `email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -55,7 +64,12 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `username`, `password`, `tipo`, `email`) VALUES
-(1, 'KAUE', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', 'escritor', 'kauemcarreiro11@gmail.com');
+(9, 'Kaue', 'kaue', 'admin', 'kauemcarreiro11@gmail.com'),
+(10, 'Ryan', 'ryan', 'escritor', 'ryan@gmail.com'),
+(11, 'Eduardo', 'eduardo', 'escritor', 'eduardo@gmail.com'),
+(12, 'Saiko', 'saiko', 'escritor', 'saiko@gmail.com'),
+(13, 'Leonardo', 'leonardo', 'admin', 'leonardo@gmail.com'),
+(15, 'Rennan', 'rennan', 'admin', 'rennan@gmail.com');
 
 --
 -- Índices para tabelas despejadas
@@ -83,13 +97,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `noticias`
 --
 ALTER TABLE `noticias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
